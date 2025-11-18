4270b2af902853eb019c0c060f993dc92d5f14be
Albert G. <a.golovanov1323@gmail.com>
2025-11-18 14:30:12 +0600

    I have prescribed the logic of adding a client to the chat, requesting his name and sending global messages.

--- EchoServer/src/ClientHandler.java
+++ EchoServer/src/ClientHandler.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -9,10 +10,14 @@ public class ClientHandler implements Runnable {
 
     private final Socket socket;
     private final Map<String, Function<String, String>> commands;
+    private final List<ClientHandler> clients;
+    private PrintWriter writer;
+    private String userName;
 
-    public ClientHandler(Socket socket, Map<String, Function<String, String>> commands) {
+    public ClientHandler(Socket socket, Map<String, Function<String, String>> commands, List<ClientHandler> clients) {
         this.socket = socket;
         this.commands = commands;
+        this.clients = clients;
     }
     @Override
     public void run() {
@@ -23,21 +28,33 @@ public class ClientHandler implements Runnable {
                 Scanner reader = getReader(socket);
                 PrintWriter writer = getWriter(socket);
         ){
-            sendResponse("Hello " + socket.getPort(), writer);
+            sendResponse("Введите ваше имя:", writer);
+            while (true) {
+                this.userName = reader.nextLine().trim();
+                if (userName.isBlank() || userName.contains(" ")){
+                    sendResponse("Имя не должно быть пустым и не должно содержать пробелов.", writer);
+                }else {
+                    break;
+                }
+            }
+            sendResponse("Hello " + userName, writer);
 
             while (true){
                 String message = reader.nextLine().trim();
                 if (isEmptyMsg(message) || isQuitMsg(message)){
                     break;
                 }
-
-                System.out.printf("Got message: %s%n", message);
-
-                String response = commands.getOrDefault(
-                        message.split(" ")[0].toLowerCase(),
-                        str -> str).apply(message);
-                System.out.printf("Sent message: %s%n", response);
-                sendResponse(response, writer);
+                if (message.startsWith("/")){
+                    String commandKey = message.substring(1).split(" ")[0].toLowerCase();
+                    if (commands.containsKey(commandKey)){
+                        String response = commands.get(commandKey).apply(message);
+                        sendResponse(response, writer);
+                    }else {
+                        sendResponse("Неизвестная команда: " + commandKey, writer);
+                    }
+                }else {
+                    broadcast(message);
+                }
             }
         }catch (NoSuchElementException e){
             System.out.println("Client dropped connection");
@@ -46,6 +63,22 @@ public class ClientHandler implements Runnable {
         }
         System.out.println("Client disconnected");
     }
+
+    public void broadcast(String msg){
+        for (ClientHandler client : clients){
+            if (client != this){
+                try {
+                    client.writer.write("<" + this.userName + ">: " + msg);
+                    client.writer.write(System.lineSeparator());
+                    client.writer.flush();
+                }catch (NoSuchElementException e){
+                    System.out.printf(client.userName + " отключился...");
+                    clients.remove(client);
+                }
+            }
+        }
+    }
+
     private PrintWriter getWriter(Socket socket) throws IOException {
         OutputStream outputStream = socket.getOutputStream();
         return new PrintWriter(outputStream);
